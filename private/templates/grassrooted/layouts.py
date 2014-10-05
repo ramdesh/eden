b4416b121e5d3f5a602903fa426ccd0c1e081781
Ramindu Deshapriya <rasade88@gmail.com>
2014-10-05 01:56:25 +0000

    Added template config for Grassrooted template

---
+++ private/templates/grassrooted/layouts.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from gluon import *
+from gluon.storage import Storage
+from s3 import *
+
+class S3MainMenuLayout(S3NavigationItem):
+    @staticmethod
+    def layout(item):
+        if not item.authorized:
+            item.enabled = False
+            item.visible = False
+        elif item.enabled is None or item.enabled:
+            item.enabled = True
+            item.visible = True
+        if item.enabled and item.visible:
+
+            items = item.render_components()
+            if item.parent is not None:
+                if item.opts.right:
+                    _class = "fright"
+                else:
+                    _class = "fleft"
+                if item.components:
+                    ## Submenu, render only if there's at list one active item
+                    if item.get_first(enabled=True):
+                        _href = item.url()
+                        return LI(DIV(A(item.label,
+                                        _href=_href,
+                                        _id=item.attr._id),
+                                        _class="hoverable"),
+                                  UL(items,
+                                     _class="submenu"),
+                                  _class=_class)
+                else:
+                    ## Menu item
+                    if item.parent.parent is None:
+                        ## Top-level item
+                        _href = item.url()
+                        if item.is_first():
+                            ## 1st item, so display logo
+                            link = DIV(SPAN(A(_href=_href),
+                                            _class="S3menulogo"),
+                                       SPAN(A(item.label, _href=_href),
+                                            _class="S3menuHome"),
+                                       _class="hoverable")
+                        else:
+                            link = DIV(A(item.label,
+                                         _href=item.url(),
+                                         _id=item.attr._id),
+                                       _class="hoverable")
+                        return LI(link, _class=_class)
+                    else:
+                        ## Submenu item
+                        if isinstance(item.label, dict):
+                            if "name" in item.label:
+                                label = item.label["name"]
+                            else:
+                                return None
+                        else:
+                            label = item.label
+                        link = A(label, _href=item.url(), _id=item.attr._id)
+                        return LI(link)
+            else:
+                ## Main menu
+                return UL(items, _id="modulenav")
+
+        else:
+            return None
